e11db5edf990ad4e45c4cb602f564c25f74c5741
C08M <bladimyr917@gmail.com>
2026-07-24 19:19:42 -0500

    Quinto y ultimo ejercicio

--- evaluacionParcial-2.js
+++ evaluacionParcial-2.js
@@ -1,4 +1,7 @@
 /* Ejercicio 1*/
+console.log("")
+console.log("Ejercicio 1")
+console.log("")
 let nombreProducto1 = "Pantalla";
 let precioProducto1 = 125;
 let stockProducto1 = true;
@@ -11,7 +14,9 @@ console.log(nombreProducto1, precioProducto1, stockProducto1);
 console.log(nombreProducto2, precioProducto2, stockProducto2);
 
 /* Ejercicio 2*/
-
+console.log("")
+console.log("Ejercicio 2")
+console.log("")
 let precio = 50;
 let descuento = 15;
 
@@ -22,7 +27,9 @@ console.log("Precio final:", precioFinal);
 console.log("Ahoro:", ahorro);
 
 /* Ejercicio 3 */ 
-
+console.log("")
+console.log("Ejercicio 3")
+console.log("")
 function edadPersona(edad) {
     if (edad < 18) {
         console.log("menor de edad");
@@ -33,10 +40,12 @@ function edadPersona(edad) {
     }
 }
 
-edadPersona(20); 
+edadPersona(45); 
 
 /* Ejercicio 4 */
-
+console.log("")
+console.log("Ejercicio 4")
+console.log("")
 let suma = 0;
 
 for (let i = 1; i <= 20; i++) {
@@ -50,3 +59,18 @@ for (let i = 1; i <= 20; i++) {
 }
 
 console.log("Suma total: " + suma);
+
+/* Ejercicio 5 */
+console.log("")
+console.log("Ejercicio 5")
+console.log("") 
+
+function mayor(numero1, numero2) {
+    if (numero1 > numero2) {
+        return numero1;
+    } else {
+        return numero2;
+    }
+}
+
+console.log("El numero mayor es: " + mayor(85716578126578165, 487149578297462897875));
